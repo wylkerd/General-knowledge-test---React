c12a604606839e412e4b2a5c249ed43bf2f24f45
wylkerd <wylkerd@hotmail.com>
2021-12-14 12:30:04 -0300

    Getting counter in sessionStorage

--- src/Pages/InitialPage.tsx
+++ src/Pages/InitialPage.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Container, Divider, Typography } from '@mui/material'
 import CounterComponent from '../Components/CounterComponent';
 import { FcIdea } from "react-icons/fc";
 
 export default function InitialPage() {
+    const [amount, setAmount] = useState(Number(window.sessionStorage.getItem('counter')));
+    useEffect(() => {console.log(amount);}, [amount]);
+    
     return (
         <>
             <Container>
@@ -11,8 +14,9 @@ export default function InitialPage() {
                 <Divider />
                 <br/><br/>
                 <Typography variant="h6" textAlign="center">Selecione a quantidade de perguntas...</Typography>
-                <div style={{'textAlign': 'center', 'marginTop': '2rem'}}><CounterComponent/></div>
-                <div style={{'textAlign': 'center', 'marginTop': '1.5rem'}}><Button variant='contained'>Avançar</Button></div>
+                <div style={{'textAlign': 'center', 'marginTop': '2rem'}} onClick={() => setAmount(Number(window.sessionStorage.getItem('counter')))} ><CounterComponent/></div>
+                { amount > 0 ? <div style={{'textAlign': 'center', 'marginTop': '1.5rem'}}><Button variant='contained'>Avançar</Button></div> : 
+                    <div style={{'textAlign': 'center', 'marginTop': '1.5rem'}}><Button variant='contained' disabled>Avançar</Button></div> }
             </Container>
         </>
     );
